44cb9eaad6335bc187461ad7f9fc14a67dfd8aa5
sakirmohammad775 <safayetsakirmohammad@gmail.com>
2024-01-27 10:10:08 +0600

    setup the tailwind,daisyUI,react router component firstly

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Home</h3>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3>blog</h3>
+        </div>
+    );
+};
+
+export default Navbar;
